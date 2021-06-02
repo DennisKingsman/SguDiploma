25e447d1a18dff13ca095006affc4baa54bdda34
Denis Kuznecov <dennis.kuznecov.1999@gmail.com>
2021-06-03 01:41:54 +0400

    s_0 and v max finds and outputs correct

--- src/main/java/com/sgu/elena/GameTheory.java
+++ src/main/java/com/sgu/elena/GameTheory.java
@@ -6,14 +6,24 @@ import java.util.Scanner;
 public class GameTheory {
 
     private static double maxV = 0;
+    private static int[] sZero;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //600
+        System.out.println("Enter total A");
+        int totalA = scanner.nextInt();
+        //300
+        System.out.println("Enter total B");
+        int totalB = scanner.nextInt();
         //2
+        System.out.println("Enter A");
         int sumA = scanner.nextInt();
         //1
+        System.out.println("Enter B");
         int sumB = scanner.nextInt();
         //3
+        System.out.println("Enter S number");
         int marketNum = scanner.nextInt();
         int[] arrayS = new int[marketNum];
         double[] arrayAlpha = new double[marketNum];
@@ -29,42 +39,52 @@ public class GameTheory {
         for (int i = 0; i < marketNum; i++) {
             arrayK[i] = scanner.nextDouble();
         }
+
         for (int i = 0; i < marketNum; i++) {
             int m = i + 1;
-//            System.out.println("M is " + m);
             int[] twinS = Arrays.copyOf(arrayS, arrayS.length);
-//            System.out.println("result");
-//            print(twinS, m);
             do {
-                countMaxV(arrayAlpha, arrayK, sumA, m);
                 System.out.println("result");
                 print(twinS, m);
-            } while (nextSet(twinS, twinS.length, m));
+            } while (nextSet(twinS, twinS.length, m, sumA, arrayAlpha, arrayK));
         }
         System.out.println("MAx val is " + maxV);
+        System.out.println("S_0 is ");
+        print(sZero, sZero.length);
 
     }
 
-    private static void countMaxV(double[] arrayAlpha, double[] arrayK, int a, int m) {
-//        double first = sumAlpha(arrayAlpha, m);
+    private static boolean nextSet(int[] arrayS, int marketNum, int m,
+                                   int a, double[] alphas, double[] koefs) {
+        //count v
         double first = 0;
         double second = 0;
         for (int i = 0; i < m; i++) {
-            first += arrayAlpha[i];
-            second += 1 / arrayK[i];
+            first += alphas[arrayS[i] - 1];
+            second += 1 / koefs[arrayS[i] - 1];
         }
-        double result = (first * a - 1) / second;
+        double result = (first * a - 1) / second; //v
+
         if (result >= maxV){
             maxV = result;
+            sZero = Arrays.copyOf(arrayS, m);
         }
-    }
-
-    private static double sumAlpha(double[] arrayAlpha, int m) {
-        double res = 0;
-        for (int i = 0; i < m; i++) {
-            res += arrayAlpha[i];
+        //C_n^k
+        for (int i = m - 1; i >= 0 ; --i) {
+            if (arrayS[i] < marketNum - m + i + 1) {
+                ++arrayS[i];
+                for (int j = i + 1; j < m; ++j) {
+                    arrayS[j] = arrayS[j - 1] + 1;
+                }
+                //find maxV and S_0
+//                if (result >= maxV){
+//                    maxV = result;
+//                    sZero = Arrays.copyOf(arrayS, arrayS.length);
+//                }
+                return true;
+            }
         }
-        return res;
+        return false;
     }
 
     private static void print(int[] array, int len) {
@@ -72,19 +92,6 @@ public class GameTheory {
             System.out.print(array[i] + " ");
         }
         System.out.println();
-    }
-
-    private static boolean nextSet(int[] arrayS, int marketNum, int m) {
-        for (int i = m - 1; i >= 0 ; --i) {
-            if (arrayS[i] < marketNum - m + i + 1) {
-                ++arrayS[i];
-                for (int j = i + 1; j < m; ++j) {
-                    arrayS[j] = arrayS[j - 1] + 1;
-                }
-                return true;
-            }
-        }
-        return false;
     }
     
 }
